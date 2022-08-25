ddca2c649570eecd75f0da32dd8e5830ad694a4b
Jimmy Andreas <jimmysignature@gmail.com>
2022-08-24 20:28:54 -0400

    Fixing decimal

--- client/src/components/PlantPage.js
+++ client/src/components/PlantPage.js
@@ -1,29 +1,29 @@
-import { useEffect, useState } from "react";
-import NewPlantForm from "./NewPlantForm";
-import PlantList from "./PlantList";
-import Search from "./Search";
+import { useEffect, useState } from 'react'
+import NewPlantForm from './NewPlantForm'
+import PlantList from './PlantList'
+import Search from './Search'
 
 function PlantPage() {
-  const [plants, setPlants] = useState([]);
-  const [searchTerm, setSearchTerm] = useState("");
+  const [plants, setPlants] = useState([])
+  const [searchTerm, setSearchTerm] = useState('')
 
   useEffect(() => {
     // no need to use http://localhost:3000 here
-    fetch("/plants")
-      .then((r) => r.json())
-      .then((plantsArray) => {
-        setPlants(plantsArray);
-      });
-  }, []);
+    fetch('/plants')
+      .then(r => r.json())
+      .then(plantsArray => {
+        setPlants(plantsArray)
+      })
+  }, [])
 
   function handleAddPlant(newPlant) {
-    const updatedPlantsArray = [...plants, newPlant];
-    setPlants(updatedPlantsArray);
+    const updatedPlantsArray = [...plants, newPlant]
+    setPlants(updatedPlantsArray)
   }
 
-  const displayedPlants = plants.filter((plant) => {
-    return plant.name.toLowerCase().includes(searchTerm.toLowerCase());
-  });
+  const displayedPlants = plants.filter(plant => {
+    return plant.name.toLowerCase().includes(searchTerm.toLowerCase())
+  })
 
   return (
     <main>
@@ -31,7 +31,7 @@ function PlantPage() {
       <Search searchTerm={searchTerm} onSearchChange={setSearchTerm} />
       <PlantList plants={displayedPlants} />
     </main>
-  );
+  )
 }
 
-export default PlantPage;
+export default PlantPage
